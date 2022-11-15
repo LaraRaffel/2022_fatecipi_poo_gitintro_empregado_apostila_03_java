d2150a130bc51a15180a1b88a085b8a7f87f812c
Lara Raffel <lara.b.r@outlook.com>
2022-11-14 22:22:10 -0300

    Quarto commit realizado. Primeiro teste concluido com sucesso.

--- Empregado.java
+++ Empregado.java
@@ -7,28 +7,56 @@ public class Empregado{
     private double comissao;
     private double bonus;
 
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
     public double calcularSalario(){
-        if (tipo == 1);
-            return salario;
-        if (tipo == 2);
-            return salario + comissao;
-        if (tipo == 3);
-            return salario + bonus;
+        if (tipo == 1){
+            return salario;}
+        if (tipo == 2){
+            return salario + salario * comissao;}
+        if (tipo == 3){
+            return salario + bonus;}
         return 0;
     }
 
     public int getIdade() {
         return idade;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setIdade(int idade) {
         this.idade = idade;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
